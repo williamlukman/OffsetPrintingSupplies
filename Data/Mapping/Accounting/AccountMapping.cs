547d8347e6e3dc41092d1487a8f00532409f8cf1
William Lukman <williamlukman@gmail.com>
2014-08-29 12:01:56 +0700

    Accounting Basic yuuhuu

---
+++ Data/Mapping/Accounting/AccountMapping.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Core.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Data.Mapping
+{
+    public class AccountMapping : EntityTypeConfiguration<Account>
+    {
+        public AccountMapping()
+        {
+            HasKey(a => a.Id);
+            Ignore(a => a.Errors);
+        }
+    }
+}
